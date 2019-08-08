e23a661db16b1cbae39e563740d0a59762bcbe2d
tamjidtayeb <tayebtamjid@gmail.com>
2019-08-08 17:12:50 +1000

    parsing json

--- generate-db.py
+++ generate-db.py
@@ -1,6 +1,27 @@
 import json
 import sys
+
 data = {}
+
+# Load json data
 with open(sys.argv[1]) as json_file:
     data = json.load(json_file)
-print(data)
+
+# Parse json data
+tickets = data["activities_data"]
+
+# Add activity to short_activity_table
+def addShort():
+    print("short")
+
+# Add activity to long_activity_table
+def addLong():
+    print("long")
+
+for ticket in tickets:
+    if (ticket["activity_type"]==0):
+        addShort()
+        print(ticket)
+    else:
+        addLong()
+        print(ticket)
